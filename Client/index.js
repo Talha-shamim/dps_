434b7150a084b16c7def8ffabecad44558a8b2f5
talha shamim <talhashamim001@gmail.com>
2021-06-13 01:45:19 +0530

    js complete

--- Client/index.js
+++ Client/index.js
@@ -2,6 +2,13 @@ const like_btn = document.querySelector('#like_btn');
 const like_count = document.querySelector('#like_count');
 const date_ = document.querySelector('#date_');
 const timer = document.querySelector('#timer');
+const light = document.querySelector('#light');
+const innerlight = document.querySelector('#innerlight');
+const switch_ = document.querySelector('#switch');
+const achievement_wrapper = document.querySelector('#achievement_wrapper');
+const achievement = document.querySelector('.achievement');
+const glow_inner = document.querySelector('.glow_inner');
+const glow_outer = document.querySelector('.glow_outer');
 const reset = document.querySelector('#reset');
 
 like_btn.addEventListener('click' , function(){
@@ -22,4 +29,17 @@ setInterval(() => {
 
 reset.addEventListener('click', function(){
     timer.textContent = 0;
+})
+
+innerlight.addEventListener('click', function(){
+    if(switch_.textContent === 'Off'){
+        switch_.textContent = 'On';
+        achievement.style.opacity = "1";
+        light.classList.add('glow_outer');
+    }
+    else {
+        switch_.textContent = 'Off'
+        achievement.style.opacity = "0";
+        light.classList.remove('glow_outer');
+    }
 })
